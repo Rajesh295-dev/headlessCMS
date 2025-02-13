c293a6b1581caa7248c083facb8da0c3b7b3e90c
Rajesh295-dev <rajeshgautam766@yahoo.com>
2025-02-13 00:44:49 -0500

    payment gateway

--- api/config/server.js
+++ api/config/server.js
@@ -1,6 +1,19 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('VERCEL_URL') ? `https://${env('VERCEL_URL')}` : undefined,
   app: {
     keys: env.array('APP_KEYS'),
   },
